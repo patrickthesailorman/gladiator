b35d52bb107a2b173b1f83c92d0a56ebfd580460
patrickthesailorman <patrickthesaiorman@gmail.com>
2016-12-09 14:04:36 -0500

    1st errors removed

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,6 @@
 class Arena
+  attr_accessor :name
+  def initialize(name)
+    @name = name.capitalize
+  end
 end
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,8 @@
 class Gladiator
+  attr_accessor :name, :weapon
+  def initialize(name, weapon)
+    @name = name
+    @weapon = weapon
+  end
+
 end
